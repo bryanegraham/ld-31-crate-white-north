02924be4b3e4d0e4d5665a07f879df7bc038fcd8
Bryan Graham <bryanegraham@gmail.com>
2014-12-07 13:53:45 -0800

    Add Score overlay, fix an issue with the player wins images stacking up

---
+++ Assets/Scripts/Screens/ScoreOverlay.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+
+class ScoreOverlay : MonoBehaviour
+{
+	public GameObject Panel;
+	public Text LeftScore;
+	public Text RightScore;
+	public Text ScoreClock;
+
+	private TimeSpan span;
+
+	void Start()
+	{
+		GameManager.Instance.GameEndListeners += OnEndGame;
+		GameManager.Instance.GameSetupListeners += OnSetupGame;
+	}
+
+	void Update()
+	{
+		if (GameManager.Instance.GameRunning)
+		{
+			LeftScore.text = GameManager.Instance.Score[(int)GameManager.Side.LEFT].ToString();
+			RightScore.text = GameManager.Instance.Score[(int)GameManager.Side.RIGHT].ToString();
+
+			span = TimeSpan.FromSeconds((double)GameManager.Instance.TimeRemaining);
+			ScoreClock.text = span.Minutes + ":" + span.Seconds;
+		}
+	}
+
+	public void OnSetupGame()
+	{
+		Panel.SetActive(true);
+	}
+
+	public void OnEndGame(int[] scores)
+	{
+		ScoreClock.text = "0:00";
+	}
+}
