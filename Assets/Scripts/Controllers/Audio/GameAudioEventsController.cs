54af889ab079e07c1d5352c85c4725630b25742f
Bryan Graham <bryanegraham@gmail.com>
2014-12-07 17:29:23 -0800

    Audio Stuffs

---
+++ Assets/Scripts/Controllers/Audio/GameAudioEventsController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+class GameAudioEventsController : MonoBehaviour
+{
+	public AudioClip CrowdFail;
+	public AudioClip CrowdWin;
+	public AudioClip BandFail;
+	public AudioClip BandWin;
+
+	public AudioClip BeginGame;
+
+	public AudioSource Crowd;
+	public AudioSource Band;
+
+	void Start()
+	{
+		GameManager.Instance.GameSetupListeners += OnGameSetup;
+		GameManager.Instance.GameEndListeners += OnGameEnd;
+	}
+
+	void OnGameSetup()
+	{
+		//Band.clip = BeginGame;
+		//Band.Play();
+	}
+
+	void OnGameEnd(int[] scores)
+	{
+		if (scores[0] == scores[1])
+		{
+			Band.clip = BandFail;
+			Crowd.clip = CrowdFail;
+		}
+		else
+		{
+			Band.clip = BandWin;
+			Crowd.clip = CrowdWin;
+		}
+
+		//Band.Play();
+		Crowd.Play();
+	}
+}
